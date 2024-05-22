2fb7595d5ece690148fc6b9c4c83eada25f5b841
zesshi <iceniki2@gmail.com>
2024-05-22 03:05:01 +0300

    Kostaff-4 Парсер резюме через GPT

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import streamlit as st
 
 from app.utils.cv_parser import get_parsed_cv
-# from app.utils.download_required_packages import download_package
-
-# download_package('libreoffice')
-# download_package('libreoffice-writer')
 
 st.markdown("<h1 style='text-align: center'>Парсер резюме</h1>", unsafe_allow_html=True)
 
